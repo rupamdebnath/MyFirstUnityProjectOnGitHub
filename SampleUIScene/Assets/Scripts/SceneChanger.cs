8dde9732b6d5ee1f7a27d8edbe46a4ccbb4c984c
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-11-30 01:32:58 +0530

    Created Branch Unity_UI_Basics, 3 buttons in main scene that takes the user to respective scene on button click and back button on each scene that brings the user back to main scene. Need to merge this local branch to main branch.

---
+++ SampleUIScene/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void OnClick1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void OnClick2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void OnClick3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+
+    public void OnClickBack()
+    {
+        SceneManager.LoadScene("MasterScene");
+    }
+}
